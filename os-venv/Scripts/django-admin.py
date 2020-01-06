8626c84fe216627766a31fd5be8170c632f16274
MHP-User <MHP-User@MB-SVR-1.DOMAIN.local>
2020-01-06 04:28:54 -0500

    Created rebates app

---
+++ os-venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\MHP-User\Desktop\workspace\os-app-repo\os-venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
